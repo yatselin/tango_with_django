e32b07252bf9a2f42db4be090193f42b03455894
yatselin <yatselin@gmail.com>
2020-02-25 16:42:06 +0300

    Chapter 3 test passed

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -2,5 +2,12 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse("Rango says hey there partner!")
-# Create your views here.
+    return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+    #return HttpResponse("Rango says hey there partner!")
+
+def about(request):
+    return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
+    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>to main</a>")
+    #return HttpResponse("<a href="/about">Rango says here is the about page.</a>")
+    #(<a href='/rango/about/'>About</a>)
+    #return HttpResponse("Rango says here is the about page.")

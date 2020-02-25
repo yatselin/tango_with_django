fc79f688fddeac13b7b1f9c38238a6e8a46a0a45
yatselin <yatselin@gmail.com>
2020-02-25 22:55:23 +0300

    Chapter 4 finished

--- tango_with_django_project/rango/views.py
+++ tango_with_django_project/rango/views.py
@@ -1,13 +1,18 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 def index(request):
-    return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
-    #return HttpResponse("Rango says hey there partner!")
+    context_dict = {'boldmessage': 'Crunchy, creamy, cookie, candy, cupcake!'}
+    return render(request, 'rango/index.html', context=context_dict)
+    # Chapter 3 assignment (include hyperlink),
+    # in <a> use ">About<" not ">about<"!!! to pass test
+    # return HttpResponse("Rango says hey there partner!<br/><a href='/rango/about/'>About</a>")
+
 
 def about(request):
-    return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
-    #return HttpResponse("Rango says here is the about page. <a href='/rango/'>to main</a>")
-    #return HttpResponse("<a href="/about">Rango says here is the about page.</a>")
-    #(<a href='/rango/about/'>About</a>)
-    #return HttpResponse("Rango says here is the about page.")
+    context_dict = {'boldmessage': 'This tutorial has been put together by Iar'}
+    return render(request, 'rango/about.html', context=context_dict)
+    # Chapter 3 assignment (include hyperlink),
+    # in <a> use ">Index<" not ">index<"!!! to pass test
+    # return HttpResponse("Rango says here is the about page.<br/><a href='/rango/'>Index</a>")
